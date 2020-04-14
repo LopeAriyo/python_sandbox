f5e8f51e1e97ad5fcd2b38533e36ea99aa5e4b59
Lope Ariyo <lopeariyo@gmail.com>
2020-04-14 20:34:50 +0100

    Practiced strings in python

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,15 +1,35 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Yemoja"
+age = 2
 
 # String Formatting
+print("Hello, my name is {name} and I am {age} years old".format(
+    name=name, age=age))
+
+
+name2 = input("What's your name?: ")
+age2 = input("Hello {name}, How old are you? ".format(name=name2))
+
+# F-Strings
+print(f"Wow {name2}! You look so young for {age2}")
 
 # String Methods
 
+greeting = "Hello, World!"
 
-greeting = "Hello Lope! How are you today?"
-print(greeting)
-response = input("Your response: ")
-questions = input("Do you have a question for me: ")
-# print(age * 365 * 24 * 60 * 60)
-print("You said: {} and your question is {}.".format(response, questions))
-# Reminder: specify what goes where for this example.
+
+print(greeting.capitalize())  # Capitalize, only the first letter
+print(greeting.upper())
+print(greeting.lower())
+print(greeting.swapcase())
+print(len(greeting))
+# case sensitive, does not modify greeting
+print(greeting.replace("World", "Lope"))
+print(greeting.count("o"))
+print(greeting.startswith("Hi"))
+print(greeting.endswith("World!"))
+print(greeting.split(" "))
+
+
+# References: https://www.w3schools.com/python/python_ref_string.asp
