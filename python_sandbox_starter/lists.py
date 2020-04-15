96bea8709d24ce73fd15a331429b910043d82dd0
Lope Ariyo <lopeariyo@gmail.com>
2020-04-15 07:03:01 +0100

    Practiced lists in python

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,18 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+numbers = [5, 10, 15, 20, 25]
+numbers2 = list((2, 4, 6, 8, 10))
+numbers3 = [*numbers2, *numbers]
+
+print(numbers3)
+
+fruits = ["Mango", "Watermelon", "Pineapple", "Coconut"]
+
+fruits.append("Pomegranate")
+fruits.insert(2, "Strawberries")
+fruits.remove("Pineapple")
+fruits.pop(3)
+fruits.reverse()
+fruits.sort()
+fruits.sort(reverse=True)
+
+print(fruits)
